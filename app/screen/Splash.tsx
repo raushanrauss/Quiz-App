e10bc7b8532fb4d1b17c46b392b6ecc30527bca8
Raushan kumar <john@example.com>
2024-12-20 15:09:42 +0530

    change font size

--- app/screen/Splash.tsx
+++ app/screen/Splash.tsx
@@ -3,10 +3,10 @@ import { View, Text, Button, Image, Pressable } from "react-native";
 
 export default function Splash({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center" }}>
+    <View style={{ flex: 1, alignItems: "center", backgroundColor: "#f0f8ff" }}>
       <Image
         source={require("../../assets/images/logo.jpeg")}
-        style={{ height: "50%", aspectRatio: 1 }}
+        style={{ height: "50%", width: "100%", borderRadius: 200 }}
       />
       <Text style={{ fontSize: 24, textAlign: "center", marginBottom: 20 }}>
         Instruction
@@ -16,19 +16,19 @@ export default function Splash({ navigation }) {
           backgroundColor: "#9f7aea",
           padding: 10,
           borderRadius: 10,
-          height: 120,
+          height: 110,
           width: 320,
           justifyContent: "center",
           alignItems: "center",
         }}
       >
-        <Text style={{ color: "white", fontSize: 20 }}>
+        <Text style={{ color: "white", fontSize: 18 }}>
           Each Quiz has four option quiz
         </Text>
-        <Text style={{ color: "white", fontSize: 20 }}>
+        <Text style={{ color: "white", fontSize: 18 }}>
           Progress will be shown at the top
         </Text>
-        <Text style={{ color: "white", fontSize: 20 }}>
+        <Text style={{ color: "white", fontSize: 18 }}>
           Score would be shown at The End
         </Text>
       </View>
